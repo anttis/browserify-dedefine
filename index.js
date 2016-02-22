3471665763c4fb0f6fc1a205ca27f6973d2f66af
Matti Vakkilainen <matti.vakkilainen@yle.fi>
2016-02-22 15:17:47 +0200

    add homage to rotta

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var through    = require("through2");
 
-var WRAP_BEGIN = '__lol_define = window.define; window.define = null;'
-var WRAP_END   = 'window.define = __lol_define;'
+var WRAP_BEGIN = '__rotta_define = window.define; window.define = null;'
+var WRAP_END   = 'window.define = __rotta_define;'
 
 module.exports = function (browserify, opts) {
     var createStream = function () {
