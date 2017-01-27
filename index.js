47c5694e0d9d418ba74a70a468b1a16709e123d1
Antti Salminen <antti.salminen@reaktor.fi>
2017-01-27 12:55:20 +0200

    Wrap bundled code in a closure and overwrite `define` in it - leave global `define` untouched

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var through    = require("through2");
 
-var WRAP_BEGIN = '__rotta_define = window.define; window.define = null;'
-var WRAP_END   = 'window.define = __rotta_define;'
+var WRAP_BEGIN = '(function () { var define = undefined;'
+var WRAP_END   = '})();'
 
 module.exports = function (browserify, opts) {
     var createStream = function () {
